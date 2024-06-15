20c811775e8bcf45f737229e742c6abaad6eb7b5
fa-bb <fabb1800@gmail.com>
2024-06-15 17:21:32 +0900

    remove Center widget

--- lib/main.dart
+++ lib/main.dart
@@ -115,132 +115,130 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
         actions: [],
       ),
-      body: Center(
-        child: ReorderableListView.builder(
-            itemCount: TickTitleList.length,
-            itemBuilder: (context, index) {
-              return Card(
-                key: Key('item $index'),
-                child: ListTile(
-                  title: TickTitleList[index].istitleEditing
-                      ? TextFormField(
-                          controller: TickTitleList[index].titleController,
-                          onFieldSubmitted: (newTitle) {
-                            setState(() {
-                              TickTitleList[index].istitleEditing = false;
-                            });
-                            TickTitleList[index].title = newTitle;
-                          },
-                        )
-                      : Align(
-                          alignment: Alignment.centerLeft, // 左寄せに設定
-                          child: Padding(
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 8.0), // クリック範囲を狭める
-                            child: TextButton(
-                              onPressed: () {
-                                setState(() {
-                                  EdidingTextfieldfalse();
-                                  TickTitleList[index].istitleEditing = true;
-                                });
-                              },
-                              child: Text(TickTitleList[index].title,
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 18,
-                                      color: Colors.black)),
-                            ),
-                          ),
-                        ),
-                  subtitle: TickTitleList[index].isdetailEditing
-                      ? TextFormField(
-                          controller: TickTitleList[index].detailController,
-                          onFieldSubmitted: (newDetail) {
-                            setState(() {
-                              TickTitleList[index].isdetailEditing = false;
-                            });
-                            TickTitleList[index].detail = newDetail;
-                          },
-                        )
-                      : Align(
-                          alignment: Alignment.centerLeft, // 左寄せに設定
-                          child: Padding(
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 8.0), // クリック範囲を狭める
-                            child: TextButton(
-                              onPressed: () {
-                                setState(() {
-                                  EdidingTextfieldfalse();
-                                  TickTitleList[index].isdetailEditing = true;
-                                });
-                              },
-                              child: Text(
-                                TickTitleList[index].detail,
-                                style: TextStyle(fontSize: 12),
-                              ),
-                            ),
-                          ),
-                        ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              TickRoll(TickTitleList[index].title)),
-                    );
-                  },
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(
-                        onPressed: () {
-                          showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return AlertDialog(
-                                title: const Text('Delete Confirmation'),
-                                content: const Text(
-                                    'Are you sure you want to delete this item?'),
-                                actions: <Widget>[
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.of(context).pop(); // ダイアログを閉じる
-                                    },
-                                    child: const Text('Cancel'),
-                                  ),
-                                  TextButton(
-                                    onPressed: () {
-                                      setState(() {
-                                        TickTitleList.removeAt(index);
-                                      });
-                                      Navigator.of(context).pop(); // ダイアログを閉じる
-                                    },
-                                    child: const Text('OK'),
-                                  ),
-                                ],
-                              );
-                            },
-                          );
+      body: ReorderableListView.builder(
+          itemCount: TickTitleList.length,
+          itemBuilder: (context, index) {
+            return Card(
+              key: Key('item $index'),
+              child: ListTile(
+                title: TickTitleList[index].istitleEditing
+                    ? TextFormField(
+                        controller: TickTitleList[index].titleController,
+                        onFieldSubmitted: (newTitle) {
+                          setState(() {
+                            TickTitleList[index].istitleEditing = false;
+                          });
+                          TickTitleList[index].title = newTitle;
                         },
-                        icon: Icon(Icons.delete),
                       )
-                    ],
-                  ),
+                    : Align(
+                        alignment: Alignment.centerLeft, // 左寄せに設定
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 8.0), // クリック範囲を狭める
+                          child: TextButton(
+                            onPressed: () {
+                              setState(() {
+                                EdidingTextfieldfalse();
+                                TickTitleList[index].istitleEditing = true;
+                              });
+                            },
+                            child: Text(TickTitleList[index].title,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                    color: Colors.black)),
+                          ),
+                        ),
+                      ),
+                subtitle: TickTitleList[index].isdetailEditing
+                    ? TextFormField(
+                        controller: TickTitleList[index].detailController,
+                        onFieldSubmitted: (newDetail) {
+                          setState(() {
+                            TickTitleList[index].isdetailEditing = false;
+                          });
+                          TickTitleList[index].detail = newDetail;
+                        },
+                      )
+                    : Align(
+                        alignment: Alignment.centerLeft, // 左寄せに設定
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 8.0), // クリック範囲を狭める
+                          child: TextButton(
+                            onPressed: () {
+                              setState(() {
+                                EdidingTextfieldfalse();
+                                TickTitleList[index].isdetailEditing = true;
+                              });
+                            },
+                            child: Text(
+                              TickTitleList[index].detail,
+                              style: TextStyle(fontSize: 12),
+                            ),
+                          ),
+                        ),
+                      ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            TickRoll(TickTitleList[index].title)),
+                  );
+                },
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: const Text('Delete Confirmation'),
+                              content: const Text(
+                                  'Are you sure you want to delete this item?'),
+                              actions: <Widget>[
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop(); // ダイアログを閉じる
+                                  },
+                                  child: const Text('Cancel'),
+                                ),
+                                TextButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      TickTitleList.removeAt(index);
+                                    });
+                                    Navigator.of(context).pop(); // ダイアログを閉じる
+                                  },
+                                  child: const Text('OK'),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      },
+                      icon: Icon(Icons.delete),
+                    )
+                  ],
                 ),
-                // Icon(Icons.book),
-                // Icon(Icons.co2),
-              );
-            },
-            onReorder: (oldIndex, newIndex) {
-              setState(() {
-                if (newIndex > oldIndex) {
-                  newIndex -= 1;
-                }
-                final item = TickTitleList.removeAt(oldIndex);
-                TickTitleList.insert(newIndex, item);
-              });
-            }),
-      ),
+              ),
+              // Icon(Icons.book),
+              // Icon(Icons.co2),
+            );
+          },
+          onReorder: (oldIndex, newIndex) {
+            setState(() {
+              if (newIndex > oldIndex) {
+                newIndex -= 1;
+              }
+              final item = TickTitleList.removeAt(oldIndex);
+              TickTitleList.insert(newIndex, item);
+            });
+          }),
       floatingActionButton: FloatingActionButton(
           onPressed: () {
             _addTickTile();
